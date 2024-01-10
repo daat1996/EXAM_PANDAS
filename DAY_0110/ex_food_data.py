96d59bffbc7e4846bb1c07f2835d4e741621f0a7
daat <tempest1996@gmail.com>
2024-01-10 20:43:20 +0900

    pandas 1월 10일

--- DAY_0110/ex_food_data.py
+++ DAY_0110/ex_food_data.py
@@ -3,22 +3,32 @@ file = 'food.txt'
 kor_food, china_food = [], []
 foods = {}
 kind = ''
-# with open(file, mode='r', encoding='utf8') as f:
-#     data = f.readline()
-#     if not data.index('*'):
-#         kind = '한식' if data[1:] == '한식' else '중식'
-#     else:
-#         if kind == '한식': kor_food.append(data)
-#         else:
-#             china_food.append(data)
+with open(file, mode='r', encoding='utf8') as f:        # 1. 리스트 만들어서 분류하는법
+    while True:
+        data = f.readline().replace('\n','')
+        if not data:
+            break
+        if not data.find('*'):
+            kind = '한식' if data[1:] == '한식' else '중식'
+        else:
+            if kind == '한식': kor_food.append(data)
+            else:
+                china_food.append(data)
+
+print(kor_food)
+print(china_food)
 
 key = ''
-with open(file, mode='r', encoding='utf8') as f:
-    data = f.readline()
-for mas in data:
-    if not data.find('*'):
-        kind = data[1:]
-        foods[data[1:]]=[]
+with open(file, mode='r', encoding='utf8') as f:        # 2. 딕셔너리로 정리하는 법
+    datas = f.readlines()
+
+for mas in datas:
+    mas = mas.replace('\n','')
+    if not mas.find('*'):
+        key = mas[1:]
+        foods[key]=[]
     else:
-        print(foods[key].append(data))
+        foods[key].append(mas)
+        print(foods)
 print(foods)
+
